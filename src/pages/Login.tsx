5de8660e91bb4743b5b420997f666365750dcdbe
Keshav Dadhich <keshavdadhich@Keshavs-MacBook-Pro-2.local>
2025-08-15 00:36:03 +0530

    fix: login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import "./login.css"
+import "./Login.css"
 
 export default function Login() {
   const [isLoading, setIsLoading] = useState(false)
